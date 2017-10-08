7e8fbfb9a4972e60b13125e8cd46445e175bfe8b
Attila Berki <attila@playingo.net>
2017-10-08 12:45:27 +0200

    init

---
+++ index.js
@@ -0,0 +1,2 @@
+import Test from './src/test';
+// Import test.web.js if is exists, else import test.js
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+const config = {
+  entry: {
+    app: './index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: 'build/'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.web.js', '.js' ]
+  }
+};
+
+module.exports = config;
